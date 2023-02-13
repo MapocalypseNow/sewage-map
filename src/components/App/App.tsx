39d4818f208eba45e08bbc91db0e47c00e1a75d7
Jonny Dawe <124621585+MapocalypseNow@users.noreply.github.com>
2023-02-13 09:09:18 +0000

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import MapViewComponent from "../mapView/MapView";
+
+function App() {
+    return <MapViewComponent></MapViewComponent>;
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./styles/index.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./components/App/App";
+import GlobalStyles from "./components/App/GlobalStyles";
+
+const container = document.getElementById("root");
+const root = createRoot(container as HTMLDivElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <GlobalStyles></GlobalStyles>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
